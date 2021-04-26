b738a0db2ab7e87c2bc69af08a808756161175e2
최원석3 [goesnow] <goesnow@nexon.co.kr>
2021-04-26 15:05:25 +0900

    create: first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import AppRouter from 'src/components/router/Router';
+
+function App(props) {
+  return (
+    <>
+      <AppRouter {...props} />
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Home = () => (
+  <div>
+    Home page
+    <div>
+      <Link to="/monitor/auth/login">zz</Link>
+    </div>
+    <div>
+      <Link to="/monitor/expired">!!</Link>
+    </div>
+  </div>
+);
+
+export default Home;
