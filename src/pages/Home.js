37466145a4f79885f5ebb9d58c0bdd5b2c273000
1seok2 <misohaha31@naver.com>
2021-04-26 22:20:55 +0900

    fix: [#1] resolve bebel async error

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,27 @@
+import axios from 'axios';
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const Home = () => (
-  <div>
-    Home page
+const Home = () => {
+  const fetch = async () => {
+    return await axios.get('https://www.naver.com');
+  };
+
+  React.useEffect(() => {
+    fetch();
+  }, []);
+
+  return (
     <div>
-      <Link to="/monitor/auth/login">zz</Link>
+      Home page
+      <div>
+        <Link to="/monitor/auth/login">zz</Link>
+      </div>
+      <div>
+        <Link to="/monitor/expired">!!</Link>
+      </div>
     </div>
-    <div>
-      <Link to="/monitor/expired">!!</Link>
-    </div>
-  </div>
-);
+  );
+};
 
 export default Home;
